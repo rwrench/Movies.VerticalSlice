fc1e3255d8d033131044c9734ed4462bd43ddc43
Richard Wrench <rwrench@gmail.com>
2025-08-15 16:45:13 -0500

    Add IMovieNamesProviderService for movie name management
    Introduces the `IMovieNamesProviderService` interface and its implementation, `MovieNamesProviderService`, to manage movie names using an `ObservableCollection<MovieNameDto>`. Updates `App.xaml.cs` to register the service as a singleton. Modifies `MoviesViewModel` and `RatingsViewModel` to utilize the new service for refreshing movie names. Removes the `LoadMovieNamesAsync` method and updates bindings in `RatingsView.xaml`. Cleans up commented-out code in `GetAllMoviesHandler`. These changes improve code organization and maintainability.

---
+++ Movies.VerticalSlice.Api.Services/Services/MovieNamesProviderService.cs
@@ -0,0 +1,41 @@
+﻿using Movies.VerticalSlice.Api.Shared.Dtos;
+using System.Collections.ObjectModel;
+
+namespace Movies.VerticalSlice.Api.Services
+{
+    public class MovieNamesProviderService : IMovieNamesProviderService
+    {
+        readonly IRatingsService _ratingsService;
+        bool _isLoaded;
+        public ObservableCollection<MovieNameDto> MovieNames { get; } = new ObservableCollection<MovieNameDto>();
+
+        public MovieNamesProviderService(IRatingsService ratingsService)
+        {
+            _ratingsService = ratingsService;
+        }
+
+        public void Clear()
+        {
+            MovieNames.Clear();
+            _isLoaded = false;
+        }
+
+        public async Task EnsureLoadedAsync()
+        {
+            if (_isLoaded) return;
+            var movieNames = await _ratingsService.GetAllMovieNamesAsync();
+            MovieNames.Clear();
+            if (movieNames != null)
+            {
+                foreach (var movie in movieNames)
+                    MovieNames.Add(movie);
+            }
+            _isLoaded = true;
+        }
+
+        public async Task RefreshAsync()
+        {
+           await EnsureLoadedAsync();
+        }
+    }
+}
