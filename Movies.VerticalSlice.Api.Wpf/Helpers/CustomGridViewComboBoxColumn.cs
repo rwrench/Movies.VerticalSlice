e82049919e19ffadf0932d32f59c0fd4193054d3
Richard Wrench <rwrench@gmail.com>
2025-08-18 19:57:26 -0500

    Add IMovieNamesProviderService and custom combo box
    Updated MoviesViewModelTests to include IMovieNamesProviderService in the MoviesViewModel constructor. Added CustomGridViewComboBoxColumn for editable combo box functionality. Replaced GridViewComboBoxColumn with the new custom column in RatingsView.xaml.

---
+++ Movies.VerticalSlice.Api.Wpf/Helpers/CustomGridViewComboBoxColumn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+
+namespace Movies.VerticalSlice.Api.Wpf.Helpers
+{
+    public class CustomGridViewComboBoxColumn : GridViewComboBoxColumn
+    {
+        public override FrameworkElement CreateCellEditElement(GridViewCell cell, object dataItem)
+        {
+            var comboBox = (RadComboBox)base.CreateCellEditElement(cell, dataItem);
+            comboBox.IsEditable = true;
+            return comboBox;
+        }
+    }
+}
